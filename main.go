ba05d5bef1614764ffc0880647fd47b60d08f3d1
Sizhuo Long <114939201+longsizhuo@users.noreply.github.com>
2023-08-09 05:19:55 +1000

    fixed bugs

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"image/color"
+	"reflect"
 	"time"
 )
 
@@ -43,25 +44,32 @@ func main() {
 	myApp := app.New()
 	myApp.Settings().SetTheme(&myTheme{})
 	myWindow := myApp.NewWindow("原神，启动！")
-	myWindow.Resize(fyne.NewSize(365*3, 280*3))
+	myWindow.Resize(fyne.NewSize(365*4, 280*4))
 	svgRes, err := fyne.LoadResourceFromPath("source/Genshin_Chinese_logo.svg")
 	if err != nil {
 		fmt.Print(err)
 	}
 	img := canvas.NewImageFromResource(svgRes)
-	// panic: img.FillMode = canvas.ImageFillOriginal // 保持原始大小
-	img.SetMinSize(img.Size()) // 设置最小大小为图像的原始大小
 
-	println(img)
-	// BUG: 无法正常显示
-	img.Resize(fyne.NewSize(365, 280))
-	img.FillMode = canvas.ImageFillContain
+	// panic: img.FillMode = canvas.ImageFillOriginal // 保持原始大小
+	//imgSize := fyne.NewSize(365, 280)
+	//img.FillMode = canvas.ImageFillContain
 	//img.FillMode = canvas.ImageFillOriginal
 	// 创建一个黑色的矩形
 	rect := canvas.NewRectangle(color.Black)
 	// 创建一个白色容器
-	bg := canvas.NewRectangle(color.White)
-	content := container.NewMax(bg, rect, img)
+	//bg := canvas.NewRectangle(color.White)
+	//println(bg)
+
+	img.Resize(fyne.NewSize(365, 280))
+	spacer := canvas.NewRectangle(color.Transparent) // 创建一个透明的矩形作为空白填充
+	spacer.Resize(fyne.NewSize(300, 300))
+	println(reflect.TypeOf(spacer).String())
+	// 设置最小高度为50
+	object := fyne.CanvasObject(spacer)
+	println(reflect.TypeOf(spacer).String())
+
+	content := container.NewBorder(object, object, object, object, img)
 	go func() {
 		for i := 0; i <= 255; i++ {
 			if i == 0 {
@@ -69,9 +77,10 @@ func main() {
 			}
 			rect.FillColor = color.NRGBA{R: uint8(i), G: uint8(i), B: uint8(i), A: 255}
 			rect.Refresh()
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 20)
 		}
 	}()
+	content = container.NewMax(content)
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
